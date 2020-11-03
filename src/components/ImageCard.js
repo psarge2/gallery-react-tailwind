be0b7f8023d53ecaba4ce88c46527dc4f87adfdf
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-03 10:31:39 -0500

    made stats dynamic. Present stat categories from api

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-const ImageCard = () => {
+const ImageCard = ({ image }) => {
     return (
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src="https://source.unsplash.com/random" alt="" className="w-full"/>
+        <img src={image.webformatURL} alt="" className="w-full"/>
         <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">Photo By Jon Doe
+          <div className="font-bold text-purple-500 text-xl mb-2">
+              Photo By {image.user}
           </div>
           <ul>
             <li>
               <strong>Views: </strong>
-              4000
+              {image.views}
             </li>
             <li>
               <strong>Downloads: </strong>
-              300
+              {image.downloads}
             </li>
             <li>
               <strong>Likes: </strong>
-              400
+              {image.likes}
             </li>
           </ul>
         </div>
