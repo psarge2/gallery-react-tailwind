b81146d34edc297cea2e00dccccc3f3fbe29c231
psarge2 <54369285+psarge2@users.noreply.github.com>
2020-11-10 13:12:09 -0500

    reinstalled npm and react serve build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 
 function App() {
@@ -15,10 +16,14 @@ function App() {
         setIsLoading(false);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text 5xl text-center mx-auto mt-32">No Images Found</h1> }
+
       {isLoading ? <h1 className="text 6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image => (
           <ImageCard key={image.id} image={image}/>
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -6,7 +6,7 @@ const ImageCard = ({ image }) => {
       <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <img src={image.webformatURL} alt="" className="w-full"/>
         <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
+          <div className="font-bold text-red-500 text-xl mb-2">
               Photo By {image.user}
           </div>
           <ul>
